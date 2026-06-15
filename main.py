960917af0fe038b4eb7743696331df55fa9e41ae
John5Cruz <john._l@hotmail.com>
2026-06-14 18:25:35 -0600

    added channels definition

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ print("Connecting to Vector CAN interface...")
 # Configuration for vector Channels (0 es CAN1, 1 es CAN2)
 bus = can.Bus(
     interface='vector',
-    channel=[1],
+    channel=[0], # List of channels to use
     bitrate=500000,
-    app_name='PythonApp' # Nombre de la aplicación que diste en Vector Hardware Config
+    app_name='PythonApp' # Name of the application
 )
 
 # Send a message on CAN bus
 msg = can.Message(arbitration_id=0x123, data=[0x11, 0x22, 0x33], is_extended_id=False)
 try:
     bus.send(msg)
-    print(f"Message sent successfully: {msg}")
+    print(f"Message sent successfully: {msg}")C
 except can.CanError:
     print("Error sending message")
 
